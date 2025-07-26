1f9ede73b18c7135fdee4c478ca370bef1658e82
Altnoir Null々 <89478699+Altnoir@users.noreply.github.com>
2025-07-26 12:25:23 +0800

    Update MiaEffects.java

--- src/main/java/com/altnoir/mia/init/MiaEffects.java
+++ src/main/java/com/altnoir/mia/init/MiaEffects.java
@@ -10,10 +10,9 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class MiaEffects {
-    public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(Registries.MOB_EFFECT,
-            MIA.MOD_ID);
-    public static final Holder<MobEffect> ABYSS_CURSE = MOB_EFFECTS.register("abyss_curse",
-            () -> new AbyssCurseEffect(MobEffectCategory.HARMFUL, 0x47311A).setBlendDuration(22));
+    public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(Registries.MOB_EFFECT, MIA.MOD_ID);
+    public static final Holder<MobEffect> ABYSS_CURSE = MOB_EFFECTS.register("abyss_curse"，  () -> 
+            new AbyssCurseEffect(MobEffectCategory.HARMFUL, 0x47311A).setBlendDuration(22));
 
     public static void register(IEventBus eventBus) {
         MOB_EFFECTS.register(eventBus);
