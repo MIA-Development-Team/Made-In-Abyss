19dcfdcb27a39a2edf18732cb14d49a5eccafa4c
lonelyicer <lonelyicer@qq.com>
2026-09-20 23:32:53 +0800

    chore: the abyss curse

--- src/main/java/com/altnoir/mementoinabyss/infrastructure/data/MiaCurseDataProvider.java
+++ src/main/java/com/altnoir/mementoinabyss/infrastructure/data/MiaCurseDataProvider.java
@@ -3,11 +3,11 @@ package com.altnoir.mementoinabyss.infrastructure.data;
 import com.altnoir.mementoinabyss.MementoInAbyss;
 import com.altnoir.mementoinabyss.impl.curse.data.CurseDataProvider;
 import com.altnoir.mementoinabyss.impl.curse.record.CurseEffect;
+import com.altnoir.mementoinabyss.infrastructure.worldgen.dimension.MiaDimensions;
 import java.util.concurrent.CompletableFuture;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.level.dimension.LevelStem;
 
 public class MiaCurseDataProvider extends CurseDataProvider {
     protected MiaCurseDataProvider(
@@ -17,8 +17,9 @@ public class MiaCurseDataProvider extends CurseDataProvider {
 
     @Override
     protected void generate(HolderLookup.Provider lookup) {
-        curse(LevelStem.OVERWORLD.identifier())
+        curse(MiaDimensions.THE_ABYSS_LEVEL.identifier())
                 .level(1)
-                .effect(new CurseEffect(MobEffects.HUNGER.getKey(), 1, 200));
+                .effect(new CurseEffect(MobEffects.HUNGER.getKey(), 1, 200))
+                .effect(new CurseEffect(MobEffects.NAUSEA.getKey(), 1, 200));
     }
 }
